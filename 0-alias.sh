ce7b448da16d3a106b71c03332f6fabd006841e7
axel regnoult <regnou.a@gmail.com>
2017-01-09 15:44:39 -0400

    fake

--- 0-alias.sh
+++ 0-alias.sh
@@ -2,6 +2,8 @@
 
 # <!-- To specify DOM to use for an element’s local DOM, use the <dom-module> element. Give the <dom-module> an id attribute that matches its element’s is property and put a <template> inside the <dom-module>. Polymer will automatically clone this template’s contents into the element’s local DOM. -->
 
+alias dev='cd /Users/axel/Documents/dev'
+
 #-------------------------------------------------------------------------------
 #  now
 #-------------------------------------------------------------------------------
